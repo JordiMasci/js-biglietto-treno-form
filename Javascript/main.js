bfaadf55ccfff52914569eb5a0ce936179103ccf
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-07-20 16:19:59 +0200

    feat: fine milestone 1

--- Javascript/main.js
+++ Javascript/main.js
@@ -8,9 +8,47 @@
 // attributi p
 const finalText = document.getElementById('final-price')
 
+let namePerson = document.getElementById('Nome-utente')
+
 const userName = document.getElementById('name-user')
 
 const userKm = document.getElementById('km-to-go')
 
 const userAge = document.getElementById('age-user')
+
+const priceKm = 0.21
+
+
+
+const buttonSend = document.getElementById('send-text')
+
+
+
+
+// FUNZIONE BUTTON
+buttonSend.addEventListener('click', function(){
+    // Nome su p
+    namePerson.innerHTML = userName.value
+
+    // Prezzo finale
+    let priceTotalKm = parseInt(userKm.value) * priceKm
+
+    // Sconto
+    if (parseInt(userAge.value) < 18){
+        let priceTotalKm = parseInt(userKm.value) * priceKm * 0.2
+        finalText.innerHTML = priceTotalKm
+    } else if (parseInt(userAge.value) > 65) { 
+        let priceTotalKm = parseInt(userKm.value) * priceKm * 0.4
+        finalText.innerHTML = priceTotalKm
+    } else {
+        finalText.innerHTML = priceTotalKm
+    }
+})
+
+
+
+
+
+
+
 
